f1961b70b40c59e286c537852e9f557395590283
Ling Tan <lingt.xyz@gmail.com>
2019-01-26 14:53:56 -0500

    Updates

---
+++ precache-manifest.0034057fac525c242d72603834ca80f6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/website/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "df8eefd1f2dbc5f3d248",
+    "url": "/website/static/js/runtime~main.df8eefd1.js"
+  },
+  {
+    "revision": "908ee1d97c239160f1e8",
+    "url": "/website/static/js/main.908ee1d9.chunk.js"
+  },
+  {
+    "revision": "fa92c112869d3657d47b",
+    "url": "/website/static/js/1.fa92c112.chunk.js"
+  },
+  {
+    "revision": "908ee1d97c239160f1e8",
+    "url": "/website/static/css/main.1e5f5ca6.chunk.css"
+  },
+  {
+    "revision": "49bd8ab77ee2c1c0b299a21554e1f464",
+    "url": "/website/index.html"
+  }
+];
